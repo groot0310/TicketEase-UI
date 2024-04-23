57e21e969a76fa98ba534b302d75f90cad1a4856
AaDil Haque <aadil10haque12@gmail.com>
2024-04-23 23:42:53 +0530

    Botton sheet ui changed, done

--- TicketEase/src/app/bottom-sheet/bottom-sheet.component.ts
+++ TicketEase/src/app/bottom-sheet/bottom-sheet.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-bottom-sheet',
   standalone: true,
-  imports: [MatListModule],
+  imports: [MatListModule, MatIconModule],
   templateUrl: './bottom-sheet.component.html',
   styleUrl: './bottom-sheet.component.scss',
 })
